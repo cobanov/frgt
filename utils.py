6f61481c5fe07c1d858ce1671b2df92f33bc0440
Mert Cobanov <mertcobanov@gmail.com>
2022-02-27 23:33:59 +0300

    db integration

--- utils.py
+++ utils.py
@@ -1,14 +1,80 @@
-import tinydb
-from tinydb import TinyDB
-from tinydb_appengine.storages import EphemeralJSONStorage
+import psycopg2
+
+conn = psycopg2.connect(
+        database="d2pqfemoj0ksmj", 
+        user='hocbijyicddzcd', 
+        password='4e5491e5f4b441c81f229e6f554c0ca047059326b5750421ae536d81ea89f0f8', 
+        host='ec2-34-233-157-9.compute-1.amazonaws.com', 
+        port= '5432'
+    )
+
+def fetch_all():
+    """
+    Fetches all the topics from the database
+    :return: A list of all the topics
+    """
+
+    # Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+
+    # Executing an MYSQL function using the execute() method
+    query = """
+            SELECT * FROM cheatsheets"""
+    cursor.execute(query)
+
+    # Commit your changes in the database
+    data = cursor.fetchall()
+
+    # Closing the connection
+    conn.close()
+    return data
 
 def get_url(topic):
-    try:
-        db = tinydb.TinyDB('./gist_db.json', storage=EphemeralJSONStorage)
-    except Exception as e:
-        print(e)
-    query = tinydb.Query()
-    result = db.get(query.name == topic)['url']
-    print(result)
-    return result
+    """
+    Gets the url of the topic
+    :return: The url of the topic
+    """
 
+    # Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+
+    # Executing an MYSQL function using the execute() method
+    query = """
+            SELECT url FROM cheatsheets WHERE topic = '{}'""".format(topic)
+    cursor.execute(query)
+
+    # Commit your changes in the database
+    url = cursor.fetchone()
+
+    # Closing the connection
+    conn.close()
+    return url
+
+def add_topic(topic, url):
+    """
+    Adds a topic to the database
+    :param topic: The topic you want to add
+    :param url: The url you want to add
+    :return: None
+    """
+
+    # Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+
+    # Executing an MYSQL function using the execute() method
+    query = """
+            INSERT INTO cheatsheets(topic, url) VALUES ('{}', '{}')""".format(topic, url)
+    cursor.execute(query)
+
+    # Commit your changes in the database
+    conn.commit()
+
+    # Closing the connection
+    conn.close()
+    
+def check_is_gist(url):
+    """
+    Checks if the url is a gist
+    :return: True if the url is a gist, False if it is not
+    """
+    return url.startswith('https://gist.githubusercontent.com/')
